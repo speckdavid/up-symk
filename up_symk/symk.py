cc3ebdb9c9d1195feec40c5aa75a6ccdb23b032f
David Speck <davidjakob.speck@gmail.com>
2023-09-22 04:27:33 +0200

    preprocessor test on macOS

--- up_symk/symk.py
+++ up_symk/symk.py
@@ -37,6 +37,7 @@ class SymKOptimalPDDLPlanner(SymKMixin, PDDLAnytimePlanner):
         self,
         symk_search_config: Optional[str] = None,
         symk_anytime_search_config: Optional[str] = None,
+        symk_driver_options: Optional[str] = None,
         symk_translate_options: Optional[List[str]] = None,
         symk_preprocess_options: Optional[List[str]] = None,
         symk_search_time_limit: Optional[str] = None,
@@ -59,6 +60,7 @@ class SymKOptimalPDDLPlanner(SymKMixin, PDDLAnytimePlanner):
             self,
             symk_search_config=symk_search_config,
             symk_anytime_search_config=symk_anytime_search_config,
+            symk_driver_options=symk_driver_options,
             symk_translate_options=symk_translate_options,
             symk_preprocess_options=symk_preprocess_options,
             symk_search_time_limit=symk_search_time_limit,
@@ -146,6 +148,9 @@ class SymKOptimalPDDLPlanner(SymKMixin, PDDLAnytimePlanner):
             assert (
                 len(problem.goals) == 0
             ), "The oversubscription engine of Symk does not support hard goals! To simulate hard goals, please assign a very high utility to the hard goals (and set the plan cost bound accordingly)."
+
+            # Only translate and preprocess
+            self._symk_driver_options = ["--translate", "--search"]
 
             # Replace search engine
             self._symk_search_config = replace_search_engine_in_config(
@@ -273,7 +278,12 @@ class SymKOptimalPDDLPlanner(SymKMixin, PDDLAnytimePlanner):
 class SymKPDDLPlanner(SymKOptimalPDDLPlanner):
     def __init__(
         self,
+        symk_search_config: Optional[str] = None,
         symk_anytime_search_config: Optional[str] = None,
+        symk_driver_options: Optional[str] = None,
+        symk_translate_options: Optional[List[str]] = None,
+        symk_preprocess_options: Optional[List[str]] = None,
+        symk_search_time_limit: Optional[str] = None,
         number_of_plans: Optional[int] = None,
         plan_cost_bound: Optional[int] = None,
         log_level: str = "info",
@@ -285,7 +295,12 @@ class SymKPDDLPlanner(SymKOptimalPDDLPlanner):
             symk_anytime_search_config = f"symk-bd(plan_selection=top_k(num_plans={input_number_of_plans},dump_plans=true),bound={input_plan_cost_bound})"
 
         super().__init__(
+            symk_search_config=symk_search_config,
             symk_anytime_search_config=symk_anytime_search_config,
+            symk_driver_options=symk_driver_options,
+            symk_translate_options=symk_translate_options,
+            symk_preprocess_options=symk_preprocess_options,
+            symk_search_time_limit=symk_search_time_limit,
             number_of_plans=number_of_plans,
             plan_cost_bound=plan_cost_bound,
             log_level=log_level,
